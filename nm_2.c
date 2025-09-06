364ebd6ed12407f30f3afe175d3e5da1682a16b9
Nishit modh <modhnishit2308@gmail.com>
2025-09-06 15:10:05 +0530

    Add files via upload

---
+++ nm_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, result;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+     result = num1 - num2;
+    printf("Result = %d\n", result);
+
+    return 0;
+}
